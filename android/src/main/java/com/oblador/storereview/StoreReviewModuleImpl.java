36b7ca07fb2f9daf9dfef288bf8983462c9b7704
lyz <>
2024-09-05 16:55:24 +0800

    android 唤起失败直接前往网页版 playstore

--- android/src/main/java/com/oblador/storereview/StoreReviewModuleImpl.java
+++ android/src/main/java/com/oblador/storereview/StoreReviewModuleImpl.java
@@ -3,6 +3,8 @@ package com.oblador.storereview;
 import android.app.Activity;
 import android.util.Log;
 import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.net.Uri;
 import java.util.Map;
 import java.util.HashMap;
 import com.google.android.play.core.review.ReviewManager;
@@ -26,11 +28,28 @@ public class StoreReviewModuleImpl {
                   manager.launchReviewFlow(currentActivity, reviewInfo);
                 } else {
                   Log.w(NAME, "Current activity is null. Unable to launch review flow.");
+                  openPlayStorePage(context);
                 }
             } else {
                 Log.w(NAME, "Requesting review failed", task.getException());
+                openPlayStorePage(context);
             }
         });
+    }
+
+    private static void openPlayStorePage(ReactApplicationContext context) {
+        Activity currentActivity = context.getCurrentActivity();
+        if (currentActivity != null) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://play.google.com/store/apps/details?id=" + context.getApplicationContext().getPackageName()));
+            if (intent.resolveActivity(currentActivity.getPackageManager()) != null) {
+                currentActivity.startActivity(intent);
+            } else {
+                Log.w(NAME, "No activity found to handle the intent.");
+            }
+        } else {
+            Log.w(NAME, "Current activity is null. Unable to open Play Store.");
+        }
     }
 
 }
